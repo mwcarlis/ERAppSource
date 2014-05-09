6f2e2aafb1d274e20356ea32fe779248a0f0723e
mwcarlis <matthew.carlis@sjsu.edu>
2014-05-08 18:50:09 -0700

    DBAdapt

--- ERApp/src/com/example/erapp/DBAdapter.java
+++ ERApp/src/com/example/erapp/DBAdapter.java
@@ -3,40 +3,5 @@ package com.example.erapp;
 
 public class DBAdapter {
 
-	
-	
-	boolean editExpense(String ID, Employee u) {
-		return false;
-	}
-	void setNewExpense(Expense a, Employee u){
-		/*
-		ParseObject ExpenseObj = new ParseObject(u.getFirstName() + u.getLastName());
-		ExpenseObj.put(KEY_FNAME, u.getFirstName());
-		ExpenseObj.put(KEY_LNAME, u.getLastName());
-		ExpenseObj.put(KEY_EMPLOYEE_ID, u.getID());
-		ExpenseObj.put(KEY_JOB_TITLE, u.getJobTitle());
-		ExpenseObj.put(KEY_RANK, u.getRank());
-		ExpenseObj.put(KEY_VENDOR, a.getVendor());
-		
-		ExpenseObj.put(KEY_VENDOR, a.getVendor());
-		//ExpenseObj.put(KEY_EXPENSE_DATE, a.getExpenseDate());
-		//ExpenseObj.put(KEY_SUBMISSION_DATE, a.getSubmissionDate());
-		ExpenseObj.put(KEY_PAYMENT_TYPE, a.getPaymentType());
-		ExpenseObj.put(KEY_AMOUNT, a.getAmount());
-		ExpenseObj.put(KEY_APPROVED, a.getApproved());
-		ExpenseObj.put(KEY_RECEIPT, a.getReceipt());
-		ExpenseObj.put(KEY_NOTES, a.getNotes());
-		ExpenseObj.saveInBackground();
-		*/
-	}
-	/*
-	ArrayList<Expense>(String ID, Employee u){
-		
-		
-	}
-	ArrayList<Employee>(String ID, Employee u){
-		
-	}
-	*/
 
 }
